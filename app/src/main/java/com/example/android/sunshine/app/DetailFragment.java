dab70c56b3aafb10cdcb12d9dd1a0d1656c2f5f0
dnaiscienne <dnaiscienne@yahoo.com>
2015-09-15 23:27:43 +0800

    notification start

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -175,11 +175,11 @@ public class DetailFragment extends Fragment
             boolean isMetric = Utility.isMetric(getActivity());
 
             double high = cursor.getDouble(COL_WEATHER_MAX_TEMP);
-            String highString = Utility.formatTemperature(getActivity(), high, isMetric);
+            String highString = Utility.formatTemperature(getActivity(), high);
             mHighTempView.setText(highString);
 
             double low = cursor.getDouble(COL_WEATHER_MIN_TEMP);
-            String lowString = Utility.formatTemperature(getActivity(), low, isMetric);
+            String lowString = Utility.formatTemperature(getActivity(), low);
             mLowTempView.setText(lowString);
 
             float humidity = cursor.getFloat(COL_WEATHER_HUMIDITY);
